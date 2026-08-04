4f4c93205e2d77b929d94ff78c22cd8408b882f2
Omar Alahmad <omralahmad489@gmail.com>
2026-08-04 14:41:54 +0300

    Basics daata types of python

---
+++ Basic_Data_Type.py
@@ -0,0 +1,16 @@
+a = "Omar"
+b = 1 
+c = 2.5 
+d = True
+e = False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+# Casting
+x = 3.5
+print(int(x)) # convert from float to int
+print(str(x))
